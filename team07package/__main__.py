368e80257c63923f2ec6afd701f5e2f38ff31166
corniab <bencornia@byui.edu>
2021-09-30 18:46:45 -0600

    added placeholders for each team member in __main__.py

--- team07package/__main__.py
+++ team07package/__main__.py
@@ -1,6 +1,18 @@
+# Add import statements from your individual python file here.
 import cornia
 
 print("Welcome to our collaborative program.")
 
-# Print greeting from Ben
+# Print greeting from Ben.
 cornia.print_greeting()
+
+# Print greeting from Mitchell.
+
+
+# Print greeting from Heidi.
+
+
+# Print greeting from Morgan.
+
+
+# Print greeting from Daniel.
